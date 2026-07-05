8496ba298472efc9007d3206f049c5697a6b1151
denfasyah <denfasyah@gmail.com>
2026-07-05 17:39:45 +0700

    setup project

---
+++ data/nav-link.ts
@@ -0,0 +1,13 @@
+export interface NavLink {
+  label: string;
+  href: string;
+}
+
+export const navLinks: NavLink[] = [
+  { label: "Home", href: "#home" },
+  { label: "About", href: "#about" },
+  { label: "Menu", href: "#menu" },
+  { label: "Testimonials", href: "#testimonials" },
+  { label: "Gallery", href: "#gallery" },
+  { label: "Contact", href: "#contact" },
+];
